a80c47dc97071a495b1c8d377e6641d45fb2823a
noppawit <noppawit.wut@gmail.com>
2022-06-09 15:39:36 +0700

    create dynamic url

--- pages/index.vue
+++ pages/index.vue
@@ -9,25 +9,19 @@
       sm8
       md6
     >
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
       <v-card>
         <v-card-title class="headline">fti expo web Game</v-card-title>
+        <v-card-text>
+          <v-btn @click="$router.push(`/result/20`)">Next</v-btn>
+        </v-card-text>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   components: {
-    Logo,
-    VuetifyLogo
   }
 }
 </script>
---
+++ pages/result/_id.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        dynamic page
+    </div>
+</template>
