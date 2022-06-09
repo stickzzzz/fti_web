bbfe3bfb54d5fe04bd6d839160ed684e6f919608
noppawit <noppawit.wut@gmail.com>
2022-06-09 15:06:59 +0700

    start project

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-xs-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">fti expo web Game</v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
